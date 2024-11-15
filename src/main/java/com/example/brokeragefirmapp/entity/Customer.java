ca97590f58b88b0a9684577d3957f18edcd4a67d
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-15 15:37:29 +0300

    create Customer

---
+++ src/main/java/com/example/brokeragefirmapp/entity/Customer.java
@@ -0,0 +1,32 @@
+package com.example.brokeragefirmapp.entity;
+
+/**
+ * @author Rayan Aksu
+ * @since 11/15/2024
+ */
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private String role; // ROLE_USER or ROLE_ADMIN
+}
