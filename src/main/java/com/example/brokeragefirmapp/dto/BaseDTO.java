cbcf5fb041fbc95a03bdf0349447cd17cb59b8d7
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-15 15:56:30 +0300

    create BaseDTO

---
+++ src/main/java/com/example/brokeragefirmapp/dto/BaseDTO.java
@@ -0,0 +1,19 @@
+package com.example.brokeragefirmapp.dto;
+
+/**
+ * @author Rayan Aksu
+ * @since 11/15/2024
+ */
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class BaseDTO {
+    private String createUser;
+    private String modifyUser;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+}
