b1496b121921aae9cbe0fc6b44b6aea6b4f9a954
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-15 15:29:08 +0300

    init commit

---
+++ src/main/java/com/example/brokeragefirmapp/BrokerageFirmAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.brokeragefirmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageFirmAppApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( BrokerageFirmAppApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/example/brokeragefirmapp/BrokerageFirmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brokeragefirmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageFirmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
