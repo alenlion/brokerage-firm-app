1d41cafddc703b7d66ba749107d1d1f7a8296bcd
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-16 13:38:46 +0300

    create DepositRequestDTO, WithdrawRequestDTO

---
+++ src/main/java/com/example/brokeragefirmapp/dto/WithdrawRequestDTO.java
@@ -0,0 +1,29 @@
+package com.example.brokeragefirmapp.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Rayan Aksu
+ * @since 11/16/2024
+ */
+
+@Getter
+@Setter
+public class WithdrawRequestDTO {
+    @NotNull(message = "Customer ID is required")
+    private Long customerId;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", inclusive = false, message = "Amount must be greater than zero")
+    private BigDecimal amount;
+
+    @NotBlank(message = "IBAN is required")
+    private String iban;
+}
+
