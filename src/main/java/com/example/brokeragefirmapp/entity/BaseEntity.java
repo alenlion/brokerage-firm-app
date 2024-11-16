c3044a6fee9c6a0f764c12ca89f3a19a482021e6
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-16 21:15:38 +0300

    active jpa audit to save user and date time when add new record or update record.

--- src/main/java/com/example/brokeragefirmapp/entity/BaseEntity.java
+++ src/main/java/com/example/brokeragefirmapp/entity/BaseEntity.java
@@ -6,6 +6,7 @@ package com.example.brokeragefirmapp.entity;
  */
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +14,14 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners( AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedBy
