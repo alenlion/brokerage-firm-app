f3251b47f7011df6547e72a549e193ee0c0ffc43
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-16 13:42:01 +0300

    change CustomerController and update depositMoney method to use request body instead of path variable

--- src/main/java/com/example/brokeragefirmapp/controller/CustomerController.java
+++ src/main/java/com/example/brokeragefirmapp/controller/CustomerController.java
@@ -1,15 +1,15 @@
 package com.example.brokeragefirmapp.controller;
 
 import com.example.brokeragefirmapp.dto.ApiResponse;
+import com.example.brokeragefirmapp.dto.DepositRequestDTO;
 import com.example.brokeragefirmapp.service.AssetService;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Rayan Aksu
@@ -17,7 +17,7 @@ import java.math.BigDecimal;
  */
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping( "/api/customers" )
 public class CustomerController {
 
     private final AssetService assetService;
@@ -26,22 +26,10 @@ public class CustomerController {
         this.assetService = assetService;
     }
 
-    @PostMapping("/{customerId}/deposit")
-    @PreAuthorize("hasRole('ADMIN') or #customerId.toString() == principal")
-    public ResponseEntity<ApiResponse<Void>> depositMoney( @PathVariable Long customerId,
-                                                           @RequestParam @DecimalMin("0.01") BigDecimal amount) {
-        assetService.depositMoney(customerId, amount);
-        ApiResponse<Void> response = new ApiResponse<>(true, "Money deposited successfully", null);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-    @PostMapping("/{customerId}/withdraw")
-    @PreAuthorize("hasRole('ADMIN') or #customerId.toString() == principal")
-    public ResponseEntity<ApiResponse<Void>> withdrawMoney(@PathVariable Long customerId,
-                                                           @RequestParam @DecimalMin("0.01") BigDecimal amount,
-                                                           @RequestParam @NotBlank String iban) {
-        assetService.withdrawMoney(customerId, amount, iban);
-        ApiResponse<Void> response = new ApiResponse<>(true, "Money withdrawn successfully", null);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+    @PostMapping( "/deposit" )
+    public ResponseEntity<ApiResponse<Void>> depositMoney( @Valid @RequestBody DepositRequestDTO depositRequestDTO ) {
+        assetService.depositMoney( depositRequestDTO.getCustomerId(), depositRequestDTO.getAmount() );
+        ApiResponse<Void> response = new ApiResponse<>( true, "Money deposited successfully", null );
+        return ResponseEntity.status( HttpStatus.OK ).body( response );
     }
 }
