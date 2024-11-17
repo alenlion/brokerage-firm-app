3c3e9443c36ecd00b0edffff28c09135b409b58a
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-18 02:49:16 +0300

    set transactional to service

--- src/main/java/com/example/brokeragefirmapp/service/CustomerServiceImpl.java
+++ src/main/java/com/example/brokeragefirmapp/service/CustomerServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.brokeragefirmapp.entity.Customer;
 import com.example.brokeragefirmapp.exception.ResourceNotFoundException;
 import com.example.brokeragefirmapp.mapper.CustomerMapper;
 import com.example.brokeragefirmapp.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
