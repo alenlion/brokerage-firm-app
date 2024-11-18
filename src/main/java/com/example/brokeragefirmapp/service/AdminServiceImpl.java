347f81a4f6e80cb4f799bd7a848b23582dda15b3
Rayan <101173484+alenlion@users.noreply.github.com>
2024-11-18 16:43:36 +0300

    cancel order

--- src/main/java/com/example/brokeragefirmapp/service/AdminServiceImpl.java
+++ src/main/java/com/example/brokeragefirmapp/service/AdminServiceImpl.java
@@ -2,9 +2,15 @@ package com.example.brokeragefirmapp.service;
 
 import com.example.brokeragefirmapp.dto.AssetDTO;
 import com.example.brokeragefirmapp.dto.OrderCreateRequest;
+import com.example.brokeragefirmapp.entity.Asset;
 import com.example.brokeragefirmapp.entity.Customer;
+import com.example.brokeragefirmapp.entity.Order;
 import com.example.brokeragefirmapp.enums.OrderSide;
+import com.example.brokeragefirmapp.enums.OrderStatus;
+import com.example.brokeragefirmapp.exception.InvalidOperationException;
 import com.example.brokeragefirmapp.mapper.AssetMapper;
+import com.example.brokeragefirmapp.repository.AssetRepository;
+import com.example.brokeragefirmapp.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -29,12 +36,15 @@ public class AdminServiceImpl implements AdminService {
     private final AssetMapper assetMapper;
 
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
+    private final AssetRepository assetRepository;
 
-    public AdminServiceImpl( AssetService assetService, AssetMapper assetMapper, OrderService orderService ) {
+    public AdminServiceImpl( AssetService assetService, AssetMapper assetMapper, OrderService orderService, OrderRepository orderRepository, AssetRepository assetRepository ) {
         this.assetService = assetService;
         this.assetMapper = assetMapper;
         this.orderService = orderService;
-
+        this.orderRepository = orderRepository;
+        this.assetRepository = assetRepository;
     }
 
     private static final String[] COMPANY_NAMES = { "Apple", "Microsoft", "Amazon", "Alphabet",
@@ -43,67 +53,59 @@ public class AdminServiceImpl implements AdminService {
             "Volkswagen Group", "UnitedHealth Group", "Agricultural Bank of China", "Chevron",
             "Samsung Electronics" };
 
-//    @Override
-//    public void matchPendingOrders() {
-//        List<Order> pendingOrders = orderRepository.findByStatus( OrderStatus.PENDING );
-//
-//        for ( Order order : pendingOrders ) {
-//            try {
-//                // Update assets accordingly
-//                if ( order.getOrderSide() == OrderSide.BUY ) {
-//                    Asset asset = assetRepository.findByCustomerIdAndAssetName( order.getCustomerId(), order.getAssetName() )
-//                            .orElseGet( () -> {
-//                                Asset newAsset = new Asset();
-//                                newAsset.setCustomerId( order.getCustomerId() );
-//                                newAsset.setAssetName( order.getAssetName() );
-//                                newAsset.setSize( BigDecimal.ZERO );
-//                                newAsset.setUsableSize( BigDecimal.ZERO );
-//                                return newAsset;
-//                            } );
-//
-//                    asset.setSize( asset.getSize().add( order.getSize() ) );
-//                    asset.setUsableSize( asset.getUsableSize().add( order.getSize() ) );
-//
-//                    assetRepository.save( asset );
-//
-//                } else if ( order.getOrderSide() == OrderSide.SELL ) {
-//                    // Increase TRY asset size and usable size
-//                    BigDecimal totalAmount = order.getPrice().multiply( order.getSize() );
-//
-//                    Asset tryAsset = assetRepository.findByCustomerIdAndAssetName( order.getCustomerId(), "TRY" )
-//                            .orElseGet( () -> {
-//                                Asset newAsset = new Asset();
-//                                newAsset.setCustomerId( order.getCustomerId() );
-//                                newAsset.setAssetName( "TRY" );
-//                                newAsset.setSize( BigDecimal.ZERO );
-//                                newAsset.setUsableSize( BigDecimal.ZERO );
-//                                return newAsset;
-//                            } );
-//
-//                    tryAsset.setSize( tryAsset.getSize().add( totalAmount ) );
-//                    tryAsset.setUsableSize( tryAsset.getUsableSize().add( totalAmount ) );
-//
-//                    assetRepository.save( tryAsset );
-//                }
-//
-//                // Update order status to MATCHED
-//                order.setStatus( OrderStatus.MATCHED );
-//                orderRepository.save( order );
-//                logger.info( "Order matched with ID: {}", order.getId() );
-//
-//            } catch (OptimisticLockingFailureException ex) {
-//                logger.error( "Optimistic locking failure while matching order ID: {}", order.getId() );
-//                throw new ConcurrentModificationException( "Asset was modified by another transaction. Please retry." );
-//            } catch (Exception ex) {
-//                logger.error( "Error while matching order ID: {}", order.getId(), ex );
-//                throw new InvalidOperationException( "Error while matching orders" );
-//            }
-//        }
-//    }
-
     @Override
     public void matchPendingOrders() {
-        
+        List<Order> pendingOrders = orderRepository.findByStatus( OrderStatus.PENDING );
+
+        for ( Order order : pendingOrders ) {
+            try {
+                // Update assets accordingly
+                if ( order.getOrderSide() == OrderSide.BUY ) {
+                    Asset asset = assetRepository.findByCustomerIdAndAssetName( order.getCustomerId(), order.getAssetName() )
+                            .orElseGet( () -> {
+                                Asset newAsset = new Asset();
+                                newAsset.setCustomerId( order.getCustomerId() );
+                                newAsset.setAssetName( order.getAssetName() );
+                                newAsset.setSize( BigDecimal.ZERO );
+                                newAsset.setUsableSize( BigDecimal.ZERO );
+                                return newAsset;
+                            } );
+
+                    asset.setSize( asset.getSize().add( order.getSize() ) );
+                    asset.setUsableSize( asset.getUsableSize().add( order.getSize() ) );
+
+                    assetRepository.save( asset );
+
+                } else if ( order.getOrderSide() == OrderSide.SELL ) {
+                    // Increase TRY asset size and usable size
+                    BigDecimal totalAmount = order.getPrice().multiply( order.getSize() );
+
+                    Asset tryAsset = assetRepository.findByCustomerIdAndAssetName( order.getCustomerId(), "TRY" )
+                            .orElseGet( () -> {
+                                Asset newAsset = new Asset();
+                                newAsset.setCustomerId( order.getCustomerId() );
+                                newAsset.setAssetName( "TRY" );
+                                newAsset.setSize( BigDecimal.ZERO );
+                                newAsset.setUsableSize( BigDecimal.ZERO );
+                                return newAsset;
+                            } );
+
+                    tryAsset.setSize( tryAsset.getSize().add( totalAmount ) );
+                    tryAsset.setUsableSize( tryAsset.getUsableSize().add( totalAmount ) );
+
+                    assetRepository.save( tryAsset );
+                }
+
+                // Update order status to MATCHED
+                order.setStatus( OrderStatus.MATCHED );
+                orderRepository.save( order );
+                logger.info( "Order matched with ID: {}", order.getId() );
+
+            } catch (Exception ex) {
+                logger.error( "Error while matching order ID: {}", order.getId(), ex );
+                throw new InvalidOperationException( "Error while matching orders" );
+            }
+        }
     }
 
     @Override
